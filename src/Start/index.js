e3664fab4defd7a6fd89dc2fa160d68e529ea1ca
GleisonEm <gleison.spfc1208@hotmail.com>
2020-05-03 13:17:22 -1200

    alter index start folder

--- src/Start/index.js
+++ src/Start/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
-//import './styless.css';
+import './styless.css';
+import logo from '.././logo.svg';
+import {Link} from 'react-router-dom';
+import Header from '../header';
+import '.././global.css';
+
 
 
 function Start() {
   return (
- <h1>Mulheres são maxistas</h1>
+    <div className="App-header">
+      <Header/>
+    
+
+    <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       <p>
+         Edite e salve o código para atualizar.
+       </p>
+
+       
+      <div className="App-action">
+      
+      <Link className="buttons" to="/main">
+        <button>
+          Login
+        </button>
+      </Link>
+        
+            
+            
+     
+       
+       <button>Register</button>
+       </div>
+     
+       
+   
+     </header>
+ 
+ </div>
+
+
+
   );
 }
 
